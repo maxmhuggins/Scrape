7f8d8b16790db3d8219d35f58edba1b7d91ae1dd
Max Huggins <max.huggins98@gmail.com>
2020-11-11 15:44:57 -0600

    Converted to object oriented and added a function that will make a modified URL for special authentication.

--- Scripts/TFSScrape.py
+++ Scripts/TFSScrape.py
@@ -9,12 +9,36 @@ This is an attempt to scrape info from tfs to automate the task report.
 
 from selenium import webdriver
 
-my_url = 'http://***REMOVED***:uCfE2ahPM8C89CZ@conw-mstf-01-pv.snaponglobal.com:8080/tfs/Embedded%20Engineering%20Collection/Agile%20Sanctuary/_backlogs/Taskboard?_a=requirements'
+
+class Scraper:
+    
+    def __init__(self, Username, Password, URL):
+        self.Username = Username
+        self.Password = Password
+        self.URL = URL
+        self.ModifiedURL = self.MakeURL(self.URL)
+    
+    def MakeURL(self, URL):
+        
+        NoHTTPURL = ''
+        for i in range(0, len(URL)):
+            if i > 6:
+                NoHTTPURL += URL[i]
+            else:
+                pass
+            
+        ModifiedURL = 'http://' + '{}:{}@'.format(self.Username, self.Password) + NoHTTPURL
+        return ModifiedURL
 
 
+Username = '***REMOVED***'
+Password = 'uCfE2ahPM8C89CZ'
+URL = 'http://conw-mstf-01-pv.snaponglobal.com:8080/tfs/Embedded%20Engineering%20Collection/Agile%20Sanctuary/_backlogs/TaskBoard/2020/Sprint%2015?_a=requirements'
+
+S = Scraper(Username, Password, URL)    
+URL = S.ModifiedURL
 driver = webdriver.Chrome()
-
-driver.get(my_url)
+driver.get(URL)
 
 
 p_element = driver.find_element_by_id(id_='vss_7')
