2bfd3a70cdcd09e7b94837f7f99fee89ee87964b
Max Huggins <max.huggins98@gmail.com>
2020-11-15 20:00:43 -0600

    Formatting changes and testing some loops to format strings in the document.

--- Scripts/LaTeXMaking.py
+++ Scripts/LaTeXMaking.py
@@ -12,13 +12,52 @@ the task report from scraped data.
 import subprocess, os
 import time
 
+strings = []
+N = 10
+
+for i in range(0,N):
+    package = ['NUMBER{}'.format(i), 'NAME{}'.format(i), 'DESCRIPTION{}'.format(i)]
+    strings.append(package)
+        
 with open('../Latex/main.tex','w') as file:
+    
     file.write('\\input{./Sections/Top}\n')
-    file.write('This should work with no errors at all!\n')
-    file.write('\\end{document}\n')
+    
+    file.write('''\\large
+                \\textsc{{Top Priority}}
+                \\normalsize''')
+                
+    file.write('\\begin{enumerate}')
+    for string in strings:
+        file.write('\\item \\hlcyan{{{}}} {}: {}'.format(string[0], string[1], string[2]))
+    file.write('\\end{enumerate}\\vspace{.5cm}')
+    
+    
+    file.write('''\\large
+                \\textsc{{New Tasks}}
+                \\normalsize''')
+
+    file.write('\\begin{enumerate}')
+    for string in strings:
+        file.write('\\item \\hlyellow{{{}}} {}: {}'.format(string[0], string[1], string[2]))
+    file.write('\\end{enumerate}\\clearpage')
+
+
+    file.write('''\\large
+                \\textsc{{Previous Tasks}}
+                \\normalsize''')
+
+    file.write('\\begin{enumerate}')
+    for string in strings:
+        file.write('\\item \\hlgreen{{{}}} {}: {}'.format(string[0], string[1], string[2]))
+    file.write('\\end{enumerate}\\vspace{.5cm}')
+    
+    file.write('\\end{document}')
+    file.close()
 
 subprocess.Popen(['rubber', '-d', 'main.tex'],  cwd="../Latex")
 time.sleep(2)
 subprocess.Popen(['rubber', '--clean', 'main.tex'],  cwd="../Latex")
-time.sleep(2)
+time.sleep(3)
+print('Opening document')
 subprocess.Popen(['okular', 'main.pdf'],  cwd="../Latex")
