cefd61f25ccd5c2608ddac592bb09a1a39ad4ec8
Max Huggins <max.huggins98@gmail.com>
2020-11-20 16:30:48 -0600

    Seperated the scraper class to clean up document

---
+++ Scripts/Scraper.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 18 10:42:39 2020
+
+@author: ***REMOVED***
+"""
+
+
+class Scrape:
+    
+    def __init__(self, Username, Password, URL):
+        self.Username = Username
+        self.Password = Password
+        self.URL = URL
+        self.ModifiedURL = self.MakeURL(self.URL)
+        self.wait = 1
+        
+    
+    def MakeURL(self, URL):
+        
+        NoHTTPURL = ''
+        for i in range(0, len(URL)):
+            if i > 6:
+                NoHTTPURL += URL[i]
+            else:
+                pass
+            
+        ModifiedURL = 'http://' + '{}:{}@'.format(self.Username, self.Password) + NoHTTPURL
+        return ModifiedURL
