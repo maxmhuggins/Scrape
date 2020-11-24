224e4c903f7b4311fd95e9876bf3bf6bee93ff17
Max Huggins <max.huggins98@gmail.com>
2020-11-24 16:47:17 -0600

    Added several changes to the latex formatting. Among other things...

--- Scripts/TFSScrape.py
+++ Scripts/TFSScrape.py
@@ -16,21 +16,23 @@ import Scraper
 #============================================================================#
 Username = '***REMOVED***'
 Password = 'uCfE2ahPM8C89CZ'
-# Sprint = input('Please input the Sprint number:')
 Sprint=17
+
+# Sprint = input('Please input the Sprint number\n >')
+# Username = input('Please input your username\n >')
+# Password = input('Please input your password\n >')
 URL = '''http://conw-mstf-01-pv.snaponglobal.com:8080/tfs/Embedded%20
 Engineering%20Collection/Agile%20Sanctuary/_backlogs/TaskBoard/2020/
 Sprint%20{}?_a=requirements'''.format(Sprint)
 #============================================================================#
 S = Scraper.Scrape(Username, Password, URL)    
-wait = S.wait
 URL = S.ModifiedURL
 #============================================================================#
 S.driver.get(URL)
-time.sleep(wait*2)
+time.sleep(S.wait)
 QueriesButton = S.driver.find_element_by_xpath('/html/body/div[2]/div/div[1]/table[2]/tbody/tr/td/div[1]/div/table/tbody/tr/td[1]/div[2]/div/ul/li[2]/a')
 QueriesButton.click()
-time.sleep(wait)
+time.sleep(S.wait*2)
 #============================================================================#
 WorkInToDo = S.driver.find_element_by_xpath('//*[@id="tfs_tnli18"]')
 WorkInToDo.click()
@@ -52,33 +54,10 @@ try:
                 
         S.ToDoCounter += 1
 except NoSuchElementException:
-    pass     
-#============================================================================#
-WorkInTest = S.driver.find_element_by_xpath('//*[@id="tfs_tnli17"]')
-WorkInTest.click()
-time.sleep(wait)
-
-S.Handler()
-
-try:
-    for element in S.elements:
-        
-        S.TaskExtractor(element)        
-        
-        Task = '\\item \\hlcyan{{{}}} {}: {}\n'.format(
-        S.TaskNumber.text, S.Person, S.TaskDescription.text)
-        
-        if S.TodaySec > S.TaskTimeSec + S.SecondsSinceLastReport:
-            S.PreviousTasks.append(Task)
-        else:
-            S.NewTasks.append(Task)
-
-except NoSuchElementException:
-    print('There are {} tasks in Test'.format(element + 1))
+    pass
 #============================================================================#
 WorkInProgress = S.driver.find_element_by_xpath('//*[@id="tfs_tnli16"]')
 WorkInProgress.click()
-time.sleep(wait)
 
 S.Handler()
 
@@ -99,9 +78,29 @@ try:
 except NoSuchElementException:
     print('There are {} tasks in To Do'.format(S.ToDoCounter))
 #============================================================================#
+WorkInTest = S.driver.find_element_by_xpath('//*[@id="tfs_tnli17"]')
+WorkInTest.click()
+
+S.Handler()
+
+try:
+    for element in S.elements:
+        
+        S.TaskExtractor(element)        
+        
+        Task = '\\item \\hlcyan{{{}}} {}: {}\n'.format(
+        S.TaskNumber.text, S.Person, S.TaskDescription.text)
+        
+        if S.TodaySec > S.TaskTimeSec + S.SecondsSinceLastReport:
+            S.PreviousTasks.append(Task)
+        else:
+            S.NewTasks.append(Task)
+
+except NoSuchElementException:
+    print('There are {} tasks in Test'.format(element + 1))
+#============================================================================#
 WorkInCompleted = S.driver.find_element_by_xpath('//*[@id="tfs_tnli9"]')
 WorkInCompleted.click()
-time.sleep(wait)
 
 S.Handler()
 
@@ -129,47 +128,53 @@ with open('../Latex/TFSTesting.tex','w') as file:
 \\begin{{center}}\n
 \\Large\n
 \\textsc{{GTPS Task Report for Sprint {}}}\\\n
-\\normalsize \\today\n
+\\normalsize \\DTMnow\n
 \\end{{center}}\\vspace{{1.5cm}}\n'''.format(Sprint))
     
     file.write('''\\large\n
 \\textsc{{New Tasks}}\n
 \\normalsize\n''')
-
-    file.write('\\begin{enumerate}\n')
+    if len(S.NewTasks) == 0:
+        file.write('\\textit{No new tasks to display.}\\vspace{.5cm}\n')
+    else:
+        file.write('\\begin{enumerate}[leftmargin=!,labelindent=5pt,itemindent=-35pt]\n')
+        
+        for Task in S.NewTasks:
+            Task = list(Task)
+            for i in range(0,len(Task)):
+                if Task[i] == '&':
+                    Task[i] = '\&'
+                else:
+                    pass
+            Task = ''.join(Task)
+            file.write(Task)
     
-    for Task in S.NewTasks:
-        Task = list(Task)
-        for i in range(0,len(Task)):
-            if Task[i] == '&':
-                Task[i] = '\&'
-            else:
-                pass
-        Task = ''.join(Task)
-        file.write(Task)
-
-
-    file.write('\\end{enumerate}\\vspace{.5cm}\n')
+    
+        file.write('\\end{enumerate}\\vspace{.5cm}\n')
 
     file.write('''\\large\n
 \\textsc{{Previous Tasks}}\n
 \\normalsize\n''')
 
-    file.write('\\begin{enumerate}\n')
+    if len(S.PreviousTasks) == 0:
+        file.write('\\textit{No previous tasks to display.}\\vspace{.5cm}\n')
+    else:
+        file.write('\\begin{enumerate}[leftmargin=!,labelindent=5pt,itemindent=-35pt]\n')
+        
+        for Task in S.PreviousTasks:
+            Task = list(Task)
+            for i in range(0,len(Task)):
+                if Task[i] == '&':
+                    Task[i] = '\&'
+                else:
+                    pass
+            Task = ''.join(Task)
+            file.write(Task)
     
-    for Task in S.PreviousTasks:
-        Task = list(Task)
-        for i in range(0,len(Task)):
-            if Task[i] == '&':
-                Task[i] = '\&'
-            else:
-                pass
-        Task = ''.join(Task)
-        file.write(Task)
+    
+        file.write('\\end{enumerate}\\vspace{.5cm}\n')
                 
 
-
-    file.write('\\end{enumerate}\\vspace{.5cm}\n')
 
     file.write('\\end{document}')
 #============================================================================#
@@ -177,29 +182,27 @@ with open('../Latex/TFSTesting.tex','w') as file:
 
 
 """
-Need to clean up some of the hard code. No need for there to be so many
-repeated lines. Also, comments of course. More importantly though I need
-some method of handling weird characters like &'s and what not. The latex
-compiler is unhappy with that and just leaves it out.
+Need to comment it out.
 
 In the end, I will need some method of storing old tasks so that the task 
 report has older tasks on it. Possibly just use a .txt file to store the
-old strings and just append them at the end of the tex file.
+old strings and just append them at the end of the tex file.   - Or maybe, I 
+dont...? I mean no one is looking at the old tasks so who gives.  
 
-need to replace wait time with an exception handler...
+See about selenium waiting for a refreshed page
 
-Lastly, need to have user input UN and PASS for security purposes
-    
+Add priority grabs and add a top priority section
+Format title with date
 """
 
-time.sleep(wait)
+time.sleep(S.wait)
 
 
 S.driver.close()
 
 subprocess.Popen(['rubber', '-d', 'TFSTesting.tex'],  cwd="../Latex")
-time.sleep(2)
+time.sleep(S.wait)
 subprocess.Popen(['rubber', '--clean', 'TFSTesting.tex'],  cwd="../Latex")
-time.sleep(3)
+time.sleep(S.wait)
 print('Opening document')
 subprocess.Popen(['okular', 'TFSTesting.pdf'],  cwd="../Latex")
