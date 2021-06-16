31f1285ed0b2c0ab855f1f76ad53e5fc866f2419
Max Huggins <max.huggins98@gmail.com>
2021-06-16 08:31:09 -0500

    Seperated dictionaries from the file for easier edits.

--- Scripts/Scraper.py
+++ Scripts/Scraper.py
@@ -12,37 +12,8 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import (NoSuchElementException,
                                         StaleElementReferenceException)
 import operator
+from config import Names, AlignerModels
 # ========================================================================== #
-"""
-This is the names dictionary. It holds all of the TFS names and assigns them
-nicknames to be displayed on the report.
-"""
-Names = {
-    "David Palos": "PALOS", "Babb, David A": "BABB", "Huggins, Max": "MAX",
-    "Patton, Matthew": "MATTHEW", "Sharma, Kuldeep": "KULDEEP",
-    "Brown, Adam C.": "ADAM", "Pulapa, Rajani K.": "RAJANI",
-    "Bansal, Aman": "AMAN", "Khurana, Simran": "SIMRAN",
-    "Pandey, Sampurnanand": "SAM", "Singh, Upasana": "UPASANA",
-    "Sellers, Eric R": "ERIC", "Sperry, Jonathan A": "JONATHAN",
-    "LeCrone, Sean": "SEAN", "Robinson, Chance W": "CHANCE",
-    "D'Agostino, Robert J": "BOB", "Verma, Anju": "ANJU",
-    "Heaver, Annika": "ANNIKA", "Robbins, Lance": "LANCE",
-    "Saxena, Kritika": "KRITIKA", "Gill, George M.": "MIKE",
-    "Boehringer, Derreck B": "BO",
-    "": "Unassigned"
-    }
-
-AlignerModels = {'All': 'Specs Testing', 'Tru-Point': 'GTPS',
-                 'EZ-ADAS': 'EZ-ADAS', 'V1200': 'V-Series',
-                 'V2000': 'V-Series', 'V2100': 'V-Series',
-                 'V2280': 'V-Series', 'V2380': 'V-Series',
-                 'V3300': 'V-Series', 'Pro32': 'Pro-Series',
-                 'Pro42': 'Pro-Series', 'Dragon': 'Dragon',
-                 'OEM': 'OEM', 'Truck': 'Truck'
-                 }
-# ========================================================================== #
-
-
 class Scrape:
 
     """
